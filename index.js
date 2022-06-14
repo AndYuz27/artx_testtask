3836c52191e2de853fa3ac492ef59b732f3e5063
AndYuz27 <yuzlov2003@gmail.com>
2022-06-14 12:35:59 +0300

    aaaaa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// тест
 const express = require('express');
 const serverRouter = require("./server/routes/mRoute");
 // const dbRouter = require("./server/routes/dbRouter");
@@ -12,76 +12,15 @@ const app = express();
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const uuid = require('uuid')
-function setRes(username, repos){
-    return `<h2>${username} has ${repos} Github repos</h2>`
-}
+const uniqID = uuid.v4()
+const authRouter = require('./authRouter')
 
-async function getRepos(req, res, next) {
+async function start(){
     try{
-        console.log("Fetching Data")
-        const {username} = req.params;
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const data = await response.json();
-        const repos = data.public_repos;
-        client.setEx(username, 3600, repos)
-        
-
-        res.send(setRes(username, repos))
-    
-    
-    }catch(err){
-    
-        console.error(err)
-        res.status(500)
+        await mongoose.connect(`mongodb+srv://andy:andy@cluster0.laxisfk.mongodb.net/?retryWrites=true&w=majority`)
+        app.listen(port, () => console.log('Server is started on port ', port))
+    }catch(e){
+        console.log(e)
     }
 }
-//Установите ограничение скорости по ip до 100 запросов в час.
-const limiter = rateLimit({
-    windowMS: 20, //15 мин
-    max: 100, //ограничение по IP за 100 req
-    message: 'Превышен лимит' // сообщение по преодолению лимита
-    
-}
-)
-
-app.get("/",limiter, (req, res) => {
-    res.send("<h1>Dream Clothes</h1>")
-});
-app.get('/repos/:username', getRepos)
-app.set("views", "./server/views");
-app.set("view engine", "pug");
-
-app.get('/api',limiter, (req, res) =>{
-    res.json({
-        message: 'Welcome to the API'
-    })
-})
-app.post('/api/posts', (req, res)=> {
-    res.json({
-        message: 'Post created...'
-    })
-
-})
-app.post('/api/login', (req, res) => {
-    const user = {
-        id: 1,
-        username: 'Andy',
-        email: 'aaaand@mail.ru'
-    }
-    jwt.sign({user: user}, 'secretkey', (err, token) => {
-        res.json({
-            token
-        })
-        console.log(res)
-    })
-})
-
-
-app.use(express.static("./public"));
-
-app.use("/", serverRouter);
-
-
-
-app.listen(port);
-console.log('Всячина\n',app,'\n сервер работает на ', port, 'порте')
+start()
